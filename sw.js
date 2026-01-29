fa6d3278354b9a7f82af34c7bc5a4c23a2791af9
odub12 <odub1281@gmail.com>
2026-01-29 15:57:58 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Provides offline capability and smart caching for the PWA
  */
 
-const CACHE_VERSION = 'letsgolf-v1.0.38';
+const CACHE_VERSION = 'letsgolf-v1.0.39';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 
